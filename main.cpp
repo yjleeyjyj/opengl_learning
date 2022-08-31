f40ec872e079bb6958215965582703cead6878a6
YoungJune Lee <yjleeyjyj@gmail.com>
2022-08-31 07:33:41 -0500

    02. Create Window and Triangle Shape (Vertices) Issue: Color not applied to Triangle, Addtional Work Required.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,125 @@
 #include<glad/glad.h>
 #include<GLFW/\glfw3.h>
 
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"	gl_position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
 int main(void)
 {
+	glfwInit();
+
+	// Tell GLFW the Version of OpenGL, Which is the version of Itself: 3.3.
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Tell GLFW which OpenGL Profile prefer to Use: CORE profile.
+	//
+	// [NOTE]
+	// Only 2 profiles exist.
+	// - CORE : only contains Modern functions (GLFW_OPENGL_CORE_PROFILE)
+	// - COMPATIBILITY : contains Modern and Outdated functions. (GLFW_OPENGL_COMPAT_PROFILE)
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f
+	};
+
+	// Create Window Object
+	// Parameter : Width, Height, Title, Use Full Screen , X(Don't Care)
+	GLFWwindow* window = glfwCreateWindow(800, 800, "MyFirstOpenGL", NULL, NULL);
+	// Quick Error Check
+	if (window == NULL)
+	{
+		std::cout << "Failed to Create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1; // check return code later
+	}
+	// Tell GLFW to show window object on screen
+	glfwMakeContextCurrent(window);
+
+	// <COLOR BACKGROUND>
+	// Load GLAD to tell OpenGL preferenced Configurations
+	gladLoadGL();
+
+	// specify the viewport of OpenGL area of the window
+	glViewport(0, 0, 800, 800); // top - left, bottom - right
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	GLuint shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+
+	GLuint VAO, VBO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	
+	glBindVertexArray(VAO);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	// Specify the color of the Background
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f); // R, G, B, transparency
+	// Clean the back buffer and assign the new color on it
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	// Swap the Back Buffer with the front buffer 
+	glfwSwapBuffers(window);
+
+	// Continue Window Operation
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	// Cleaning Up Processes
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
